c2178e05cdc51e1ad4d31adcd9ffa618227b97fd
ha-lins <shuailin97@gmail.com>
2020-05-18 17:39:42 +0800

    Upload project

---
+++ before_mds/allennlp/allennlp/training/__init__.py
@@ -0,0 +1,7 @@
+from allennlp.training.callback_trainer import CallbackTrainer
+from allennlp.training.no_op_trainer import NoOpTrainer
+from allennlp.training.trainer import Trainer
+from allennlp.training.trainer_base import TrainerBase
+from allennlp.training.metatrainer import MetaTrainer
+from allennlp.training.meta_pieces import MetaTrainerPieces
+from allennlp.training.trainer_pieces import TrainerPieces
